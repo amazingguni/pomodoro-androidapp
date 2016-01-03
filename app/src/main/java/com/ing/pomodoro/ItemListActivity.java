8f9236f80bb3f1f926a57f435314b9e7acebd39d
amazingguni <amazingguni@gmail.com>
2016-01-03 17:53:32 +0900

    update auth

--- app/src/main/java/com/ing/pomodoro/ItemListActivity.java
+++ app/src/main/java/com/ing/pomodoro/ItemListActivity.java
@@ -39,8 +39,8 @@ public class ItemListActivity extends AppCompatActivity
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
+        Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(loginIntent);
       }
     });
 
